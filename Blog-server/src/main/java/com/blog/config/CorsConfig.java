f12f71bdba1b3601032d26dd527151335e6bcec5
Diamond <2210054954@qq.com>
2025-06-11 10:14:26 +0800

    跨域代码开发

--- Blog-server/src/main/java/com/blog/config/CorsConfig.java
+++ Blog-server/src/main/java/com/blog/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.blog.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,19 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        // 添加映射路径
-//        registry.addMapping("/**")
-//                // 允许哪些域的请求，星号代表允许所有
-//                .allowedOrigins("*")
-//                // 允许的方法
-//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-//                // 允许的头部设置
-//                .allowedHeaders("*")
-//                // 是否发送cookie
-//                .allowCredentials(true)
-//                // 预检间隔时间
-//                .maxAge(168000);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
